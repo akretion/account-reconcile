92b69fcfb3a89c4e9de17b6d4ce1e068f7e0964c
Sébastien Beau <sebastien.beau@akretion.com>
2013-05-02 06:57:23 +0200

    commit pending merge

--- account_statement_base_import/parser/parser.py
+++ account_statement_base_import/parser/parser.py
@@ -27,10 +27,14 @@ def UnicodeDictReader(utf8_data, **kwargs):
     pos = utf8_data.tell()
     sample_data = utf8_data.read(1024)
     utf8_data.seek(pos)
-    dialect = sniffer.sniff(sample_data, delimiters=',;\t')
+    if not kwargs.get('dialect'):
+        dialect = sniffer.sniff(sample_data, delimiters=',;\t')
+        del kwargs['dialect']
+    else:
+        dialect = kwargs.pop('dialect')
     csv_reader = csv.DictReader(utf8_data, dialect=dialect, **kwargs)
     for row in csv_reader:
-        yield dict([(key, unicode(value, 'utf-8')) for key, value in row.iteritems()])
+        yield dict([(unicode(key, 'utf-8'), unicode(value, 'utf-8')) for key, value in row.iteritems()])
 
 
 class BankStatementImportParser(object):
