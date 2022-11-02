b2e35c89e85fb0d1a3c854bd68bc89623d79fe18
Enric Tobella <etobella@creublanca.es>
2022-11-03 00:34:30 +0100

    fixup! Improve views: hide buttons, solve a false value

--- account_reconcile_oca/static/src/js/reconcile_data_widget.esm.js
+++ account_reconcile_oca/static/src/js/reconcile_data_widget.esm.js
@@ -1,12 +1,41 @@
 /** @odoo-module **/
 
+import fieldUtils from "web.field_utils";
+import session from "web.session";
 import {registry} from "@web/core/registry";
 
 const {Component} = owl;
 
 export class AccountReconcileDataWidget extends Component {
     getReconcileLines() {
-        return this.props.record.data[this.props.name].data;
+        var data = this.props.record.data[this.props.name].data;
+        for (var line in data) {
+            data[line].amount_format = fieldUtils.format.monetary(
+                data[line].amount,
+                undefined,
+                {
+                    currency: session.get_currency(data[line].currency_id),
+                }
+            );
+            data[line].debit_format = fieldUtils.format.monetary(
+                data[line].debit,
+                undefined,
+                {
+                    currency: session.get_currency(data[line].currency_id),
+                }
+            );
+            data[line].credit_format = fieldUtils.format.monetary(
+                data[line].credit,
+                undefined,
+                {
+                    currency: session.get_currency(data[line].currency_id),
+                }
+            );
+            data[line].date_format = fieldUtils.format.date(
+                fieldUtils.parse.date(data[line].date, undefined, {isUTC: true})
+            );
+        }
+        return data;
     }
     selectReconcileLine(ev, line) {
         this.props.record.update({
@@ -14,6 +43,7 @@ export class AccountReconcileDataWidget extends Component {
             manual_account_id: line.account_id,
             manual_name: line.name,
             manual_amount: line.amount,
+            manual_partner_id: line.partner_id,
         });
     }
 }
